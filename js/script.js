a89fa7e022db69ca967c40cdedacca7124e1fba1
sajedul-dev <sajedulislamraja@gmail.com>
2024-01-28 16:38:50 +0600

    back to top btn added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ========= back to top operation ============= 
+
+$(function(){
+    $(".b_t").click(function(){
+        $("body,html").animate({scrollTop:0},1500);
+    })
+
+    // ------------ navbar fixed operaion ------------ 
+});
